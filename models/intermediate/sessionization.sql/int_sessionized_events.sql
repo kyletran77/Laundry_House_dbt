459f099f7cea4efa0f592c234bd3246e08c412a9
Kyle Tran <48851847+kyletran77@users.noreply.github.com>
2024-12-11 14:58:14 -0800

    Update: remove tracks for staging and int

--- models/intermediate/sessionization.sql/int_sessionized_events.sql
+++ models/intermediate/sessionization.sql/int_sessionized_events.sql
@@ -1,70 +1,69 @@
 with segment_events_combined as (
--- combine all enabled events tables into one combined events tables
-select
-  track_events.event_timestamp as timestamp,
-  user_id,
-  anonymous_id,
-  track_id,
-  null as page_view_id
-from 
-  {{ref('stg_tracks_sgtm')}} as track_events
-union all 
-select
-  page_events.page_view_timestamp as timestamp,
-  user_id,
-  anonymous_id,
-  null as track_id,
-  page_view_id
-from 
-  {{ref('stg_page_views')}} as page_events
+    select
+        page_view_timestamp as timestamp,
+        user_id,
+        anonymous_id,
+        page_view_id
+    from 
+        {{ref('stg_page_views')}} as page_events
 ),
 
 segment_events_mapped as (
--- map anonymous_id to user_id (where possible)
-select
-  segment_events_combined.timestamp,
-  anonymous_id,
-
-  coalesce(
-    segment_events_combined.user_id,
-    segment_user_anonymous_map.user_id,
-    segment_events_combined.anonymous_id
-  ) as blended_user_id,
-  track_id,
-  page_view_id
-from
-  segment_events_combined
-  left join {{ref('stg_users_map')}} as segment_user_anonymous_map
-   using(anonymous_id)
+    -- map anonymous_id to user_id (where possible)
+    select
+        segment_events_combined.timestamp,
+        anonymous_id,
+        coalesce(
+            segment_events_combined.user_id,
+            segment_user_anonymous_map.user_id,
+            segment_events_combined.anonymous_id
+        ) as blended_user_id,
+        page_view_id
+    from
+        segment_events_combined
+        left join {{ref('stg_users_map')}} as segment_user_anonymous_map
+            using(anonymous_id)
 ),
 
 session_starts as (
--- label the event that starts the session
-select
-  *,
-  coalesce(
-    (
-      timestamp_diff(
-    segment_events_mapped.timestamp , lag(timestamp ) over (partition by blended_user_id order by timestamp asc ), millisecond)
-    ) >= 1800000,
-    true
-  ) as session_start_event
-from
-  segment_events_mapped
+    -- label the event that starts the session
+    select
+        *,
+        coalesce(
+            (
+                timestamp_diff(
+                    timestamp,
+                    lag(timestamp) over (partition by blended_user_id order by timestamp asc),
+                    millisecond
+                ) >= 1800000
+            ),
+            true
+        ) as session_start_event
+    from
+        segment_events_mapped
 ),
 
 with_session_index as (
--- add a session_index (users first session = 1, users second session = 2 etc)
-select
-  *,
-  sum(case when session_start_event then 1 else 0 end ) over (partition by blended_user_id order by session_starts.timestamp asc rows between unbounded preceding and current row) as session_index
-from
-  session_starts
+    -- add a session_index (users first session = 1, users second session = 2 etc)
+    select
+        *,
+        sum(case when session_start_event then 1 else 0 end) over (
+            partition by blended_user_id 
+            order by timestamp asc 
+            rows between unbounded preceding and current row
+        ) as session_index
+    from
+        session_starts
 )
 
 -- add a unique session_id to each session
 select
-  *,
-  cast(farm_fingerprint(concat(cast(session_index as string),cast(blended_user_id as string))) as string) as session_id
+    *,
+    cast(farm_fingerprint(
+        concat(
+            cast(session_index as string),
+            cast(blended_user_id as string)
+        )
+    ) as string) as session_id
 from
-  with_session_index
+    with_session_index
--- models/intermediate/sessionization.sql/int_sessionized_page_views.sql
+++ models/intermediate/sessionization.sql/int_sessionized_page_views.sql
@@ -1,47 +1,47 @@
 with page_views_w_sessions as (
     select
-  sessionized_events.session_id,
-  sessionized_events.timestamp,
-  sessionized_events.blended_user_id,
-  sessionized_events.anonymous_id,
-  sessionized_events.session_index,
-  sessionized_events.page_view_id,
-  page_views.page_url,
-  page_views.page_referrer,
-  page_views.page_title,
-  page_views.page_query,
-  page_views.page_path,
-  page_views.page_host, 
-  page_views.page_referrer_host,  
-  page_views.page_path_host,
-  page_views.utm_content,
-  page_views.utm_medium,
-  page_views.utm_source,
-  page_views.utm_campaign,
-  page_views.utm_term,
-  page_views.utm_id,
-  page_views.channel_source,
-  page_views.channel_medium,
-  page_views.referrer_source,
-  page_views.referrer_medium,
-  page_views.device_browser_brand,
-  page_views.device_is_mobile,
-  page_views.device_platform,
-  page_views.geo_city,
-  page_views.geo_country,
-  page_views.geo_region  
-from 
-  {{ref('int_sessionized_events')}} as sessionized_events
-  left join {{ref('stg_page_views')}} as page_views
-    on sessionized_events.page_view_id = page_views.page_view_id
-where
-  sessionized_events.page_view_id is not null
-
+        sessionized_events.session_id,
+        sessionized_events.timestamp as page_view_timestamp,
+        sessionized_events.blended_user_id,
+        sessionized_events.anonymous_id,
+        sessionized_events.session_index,
+        sessionized_events.page_view_id,
+        -- page information
+        page_views.page_url,
+        page_views.page_referrer,
+        page_views.page_title,
+        page_views.page_query,
+        page_views.page_path,
+        page_views.page_host, 
+        page_views.page_referrer_host,  
+        page_views.page_path_host,
+        -- marketing related fields
+        page_views.utm_content,
+        page_views.utm_medium,
+        page_views.utm_source,
+        page_views.utm_campaign,
+        page_views.utm_term,
+        page_views.utm_id,
+        -- channel grouping
+        page_views.channel_source,
+        page_views.channel_medium,
+        page_views.referrer_source,
+        page_views.referrer_medium,
+        -- device information
+        page_views.device_browser_brand,
+        page_views.device_is_mobile,
+        page_views.device_platform,
+        -- geo information
+        page_views.geo_city,
+        page_views.geo_country,
+        page_views.geo_region  
+    from 
+        {{ref('int_sessionized_events')}} as sessionized_events
+        left join {{ref('stg_page_views')}} as page_views
+            using(page_view_id)
 )
 
-
-SELECT * from
-page_views_w_sessions
+select * from page_views_w_sessions
 
 
 
