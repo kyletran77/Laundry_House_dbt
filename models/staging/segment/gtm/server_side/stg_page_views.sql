cf161b538fc83fbd0247d7f84a8c6228a2e56442
Kyle Tran <48851847+kyletran77@users.noreply.github.com>
2024-12-11 15:12:35 -0800

    Update: Remove referrer mapping

--- models/staging/segment/gtm/server_side/stg_page_views.sql
+++ models/staging/segment/gtm/server_side/stg_page_views.sql
@@ -29,100 +29,52 @@ with
 page_views as (
     select
         -- user properties
-    anonymous_id,
-    user_id,
+        anonymous_id,
+        user_id,
 
-    -- contextual properties
-    context_geo_city as geo_city,
-    context_geo_country as geo_country,
-    context_geo_region as geo_region,
-    context_ip as ip_address,
-    context_screen_screen_resolution as screen_screen_resolution,
-    context_screen_viewport_size as screen_viewport_size,
-    context_user_agent as user_agent,
-    -- context_user_agent_data_brands as user_agent_brands,
-    context_user_agent_data_browser_brand as device_browser_brand,
-    -- context_user_agent_data_browser_version as device_browser_version,
-    -- context_user_agent_data_engine_name as device_engine_name,
-    -- context_user_agent_data_engine_version as device_engine_version,
-    context_user_agent_data_mobile as device_is_mobile,
-    context_user_agent_data_platform as device_platform,
+        -- contextual properties
+        context_geo_city as geo_city,
+        context_geo_country as geo_country,
+        context_geo_region as geo_region,
+        context_ip as ip_address,
+        context_screen_screen_resolution as screen_screen_resolution,
+        context_screen_viewport_size as screen_viewport_size,
+        context_user_agent as user_agent,
+        context_user_agent_data_browser_brand as device_browser_brand,
+        context_user_agent_data_mobile as device_is_mobile,
+        context_user_agent_data_platform as device_platform,
 
-    -- page properties
-    context_page_path as page_path,
-     concat(context_page_path , {{ dbt_utils.get_url_host("context_page_url") }}) as page_path_host,
-   
-    context_page_referrer as page_referrer,
-    replace({{ dbt_utils.get_url_host("referrer") }}, 'www.', '') as page_referrer_host,
-    context_page_search as page_query,
-    context_page_title as page_title,
-    context_page_url as page_url,
-    {{ dbt_utils.get_url_host("context_page_url") }} as page_host,
-    -- marketing related fields 
-    {{
-        escape_temporary_utm_adjustment(
-            "context_campaign_source",
-            "context_campaign_medium",
-            "context_campaign_campaign",
-            "context_campaign_term",
-            "context_campaign_content",
-            "context_page_url",
-        )
-    }},
+        -- page properties
+        context_page_path as page_path,
+        concat(context_page_path, {{ dbt_utils.get_url_host("context_page_url") }}) as page_path_host,
+        context_page_referrer as page_referrer,
+        replace({{ dbt_utils.get_url_host("referrer") }}, 'www.', '') as page_referrer_host,
+        context_page_search as page_query,
+        context_page_title as page_title,
+        context_page_url as page_url,
+        {{ dbt_utils.get_url_host("context_page_url") }} as page_host,
 
-    {{ dbt_utils.get_url_parameter("context_page_url", "utm_id") }} as utm_id,
+        -- marketing related fields 
+        context_campaign_source as utm_source,
+        context_campaign_medium as utm_medium,
+        context_campaign_name as utm_campaign,
+        context_campaign_term as utm_term,
+        context_campaign_content as utm_content,
+        {{ dbt_utils.get_url_parameter("context_page_url", "utm_id") }} as utm_id,
 
-    {{ get_all_click_ids("context_page_url") }},
+        -- event properties
+        sent_at as page_view_timestamp,
+        id as page_view_id,
 
-    -- event properties
-    sent_at as page_view_timestamp,
-    id as page_view_id,
+        -- default channel values
+        '(direct)' as channel_source,
+        '(none)' as channel_medium,
+        null as referrer_source,
+        null as referrer_medium
 
-from base_source
-),
-referrer_mapping as (
-
-    select * from {{ ref('referrer_mapping') }}
-
-),
- page_views_mapped_referrer as (
-
-    select
-        page_views.*,
-        lower(referrer_mapping.medium) as referrer_medium,
-        lower(referrer_mapping.source) as referrer_source
-
-    from page_views as page_views
-
-    left join referrer_mapping on page_views.page_referrer_host = referrer_mapping.host
-
-),
-
-mapped_refferer_with_channel_group as (
-    select 
-
-    distinct
-    
-    page_views_mapped_referrer.*,
-    coalesce(
-        utm_source,
-        CASE
-            WHEN referrer_source IS NOT NULL 
-                 AND NOT referrer_source IN UNNEST({{ var('domain_hosts') | tojson | safe }})
-                 AND NOT referrer_source IN UNNEST({{ var('referral_exclusions') | tojson | safe }})
-            THEN referrer_source
-            WHEN page_referrer_host IS NOT NULL 
-                 AND NOT page_referrer_host IN UNNEST({{ var('domain_hosts') | tojson | safe }})
-                 AND NOT page_referrer_host IN UNNEST({{ var('referral_exclusions') | tojson | safe }})
-            THEN page_referrer_host
-            ELSE '(direct/none)'
-        END
-    ) as channel_source,
-    coalesce(utm_medium, referrer_medium) as channel_medium
-    from page_views_mapped_referrer
+    from base_source
 )
 
-
-select * from mapped_refferer_with_channel_group 
+select * from page_views
 
 
